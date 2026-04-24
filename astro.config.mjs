72fb95d3a0b41b01258064a0dd87eba17a0ed696
LUIS - C0D3BREAK3R <hackermatrx1@gmail.com>
2026-04-23 20:41:21 -0400

    feat: actualizar favicon, agregar fuente local y mejorar layout del header
    Se agregó un nuevo favicon PNG y se eliminó el antiguo favicon SVG.
    
    Se incorporó una variable font local (Inter) para mejorar la tipografía.
    
    Se actualizó el layout del header para incluir el nuevo favicon y se ajustaron los estilos para mejor alineación y espaciado.
    
    Se modificaron los botones de llamada a la acción para mayor consistencia y mejor experiencia de usuario.
    
    Se actualizaron los metadatos de Open Graph para mejorar el SEO y el compartido en redes sociales.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://luisenrique.dev",
     compressHTML: true,
     integrations: [
+        sitemap(),
         icon({
             include: {
                 lucide: ["*"],
